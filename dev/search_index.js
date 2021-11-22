fa074c45afb87170f99814fbc9d76f94952615c3
Documenter.jl <documenter@juliadocs.github.io>
2021-11-22 16:50:00 +0000

    build based on b6656b9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AOC2021","category":"page"},{"location":"#AOC2021","page":"Home","title":"AOC2021","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AOC2021.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AOC2021]","category":"page"}]
+}
