e7c524893f592c627728c2ccf6d849d5cf7b61ee
Brynjar Smári Bjarnason <binni@binnisb.com>
2021-12-02 13:32:54 +0100

    Type for the day solve not needed

--- src/AOC2021.jl
+++ src/AOC2021.jl
@@ -15,16 +15,16 @@ module AOC2021
     read(::Type{TestInput}, num::Int) = read(Puzzle, "/home/binni/projects/AdventOfCode2021/data/exp_raw/puzzle_test/$(num).txt")
 
 
-    solve(::Type{T}, number, part) where T <: Puzzle = read(T,number) |> x->solve(T, Val(number), Val(part), x)
+    solve(::Type{T}, number, part) where T <: Puzzle = read(T,number) |> x->solve( Val(number), Val(part), x)
     
     
-    solve(::Type{T}, ::Val{1}, input, shift=1) where T <: Puzzle = map(x->parse(Int,x), input) |> nums->
+    solve(::Val{1}, input, shift=1) = map(x->parse(Int,x), input) |> nums->
         sum(1 for i in (shift+1):length(nums) if nums[i] > nums[i-shift])
 
-    solve(::Type{T}, ::Val{1}, ::Val{1}, input) where T <: Puzzle = solve(T,Val(1),input, 1)
-    solve(::Type{T}, ::Val{1}, ::Val{2}, input) where T <: Puzzle = solve(T,Val(1),input, 3)
+    solve(::Val{1}, ::Val{1}, input) = solve(Val(1),input, 1)
+    solve(::Val{1}, ::Val{2}, input) = solve(Val(1),input, 3)
 
-    solve(::Type{T}, ::Val{2}, input) where T <: Puzzle = begin
+    solve(::Val{2}, input) = begin
         global forward = 0
         global depth = 0
         global aim = 0
@@ -42,6 +42,6 @@ module AOC2021
         end
         (forward=forward, depth=depth, aim=aim)
     end
-    solve(::Type{T}, ::Val{2}, ::Val{1}, input) where T <: Puzzle = solve(T, Val(2), input) |> x-> x.forward*x.aim
-    solve(::Type{T}, ::Val{2}, ::Val{2}, input) where T <: Puzzle = solve(T, Val(2), input) |> x-> x.forward*x.depth
+    solve(::Val{2}, ::Val{1}, input) = solve(Val(2), input) |> x-> x.forward*x.aim
+    solve(::Val{2}, ::Val{2}, input) = solve(Val(2), input) |> x-> x.forward*x.depth
 end
